6fa83a714c859e54dcbeba956db505967f398e88
Adam Konieczny <az_k@gazeta.pl>
2018-06-09 11:27:30 +0200

    singleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
 import facade.Facade;
+import singleton.User;
 import strategy.*;
 
 public class Main {
     public static void main(String[] args) {
 //        facade();
-        strategy();
+//        strategy();
+//        singleton2();
+//        singleton();
+        
 
+    }
+
+    private static void singleton2() {
+        User user = User.getInstance();
+        user.setAge(16);
+        user.setName("Michał");
+    }
+
+    private static void singleton() {
+        User user = User.getInstance();
+        System.out.println(user.toString());
+        System.out.println(user.hashCode());
     }
 
     private static void strategy() {
